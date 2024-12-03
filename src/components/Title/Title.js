434af346cdb77a12907fa327c1cc05733bd7df28
NCHUMA <nicachuma@gmail.com>
2024-12-03 19:39:44 -0300

    Se remueve userSelect al titulo y subtitulo

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -5,7 +5,7 @@ import "./Title.css";
 const Title = () => {
   const { dictionary } = useLanguage();
   return (
-    <div className="title">
+    <div className="title" style={{userSelect: 'none'}}>
       <h1 className="black tracked">FocusAI</h1>
       <h2 className="black tracked f5-l f7">{dictionary.subtitle}</h2>
     </div>
